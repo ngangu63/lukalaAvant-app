19befac2c6dc7ac7302acf50981cfe016f6a213c
ngangu63 <banabalukala@gmail.com>
2026-01-20 14:49:01 -0800

    Koffi Koffiento

--- src/app/big-picture.service.ts
+++ src/app/big-picture.service.ts
@@ -39,6 +39,7 @@ export class BigPictureService {
     { name: 'Samy Kedo', role: '', location: 'DR Congo', imageUrl: 'assets/images/membres/SamyKedo.jpeg', gender: 1, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
     { name: 'Valentin Kitemoko', role: '', location: 'Allemagne', imageUrl: 'assets/images/membres/ValentinKitemoko.jpeg', gender: 1, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
     { name: 'Pierre Bania', role: '', location: 'DR Congo', imageUrl: 'assets/images/membres/BaniaPiere.jpeg', gender: 1, defaultImage: 'assets/images/Remy_Cravate1.jpg' , profession: '', message: '', LinkedIn: '' },
+    { name: 'Tio Koffi Koffiento', role: '', location: 'Canada', imageUrl: 'assets/images/membres/KoffiKoffiento.jpeg', gender: 1, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
     { name: 'Fidele Menavanza', role: '', location: 'États-Unis', imageUrl: '', gender: 1, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
     { name: 'Clémentine Buaka', role: '', location: 'France', imageUrl: '', gender: 0, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
     { name: 'Constant Lutonda', role: '', location: 'Brésil', imageUrl: '', gender: 1, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
@@ -82,8 +83,7 @@ export class BigPictureService {
     { name: 'Anaclet Masamba', role: '', location: 'DR Congo', imageUrl: '', gender: 1, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
     { name: 'Rachel Bakala', role: '', location: 'France', imageUrl: '', gender: 0, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
     { name: 'Moïse Fuakuingi', role: '', location: 'Angleterre', imageUrl: '', gender: 1, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
-    { name: 'Tio Koffi Koffiento', role: '', location: 'Canada', imageUrl: '', gender: 1, defaultImage: 'assets/images/Remy_Cravate1.jpg', profession: '', message: '', LinkedIn: '' },
-
+   
   ];
 
 
