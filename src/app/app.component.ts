6857ccd18d811e9668ec2b1f09a9fad79369f9fd
ngangu63 <banabalukala@gmail.com>
2026-04-08 23:50:12 -0700

    musics reformating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { MyfooterComponent } from "./myfooter/myfooter.component";
+import { MyfooterComponent } from './myfooter/myfooter.component';
 import { RouterModule } from '@angular/router';
 import { OthersMembersComponent } from './others-members/others-members.component';
 import { CommonModule } from '@angular/common';
@@ -10,34 +10,37 @@ import { BigPictureService } from './big-picture.service';
 import { FormsModule } from '@angular/forms';
 import { Observable, of } from 'rxjs';
 
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyfooterComponent, RouterModule, CommonModule, FormsModule],
+  imports: [
+    RouterOutlet,
+    MyfooterComponent,
+    RouterModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-
-
   title = 'myDropDown-app';
   isSearchEnabled: boolean = true;
-  searchQuery: string = "";
+  searchQuery: string = '';
   searchResults: BigPicture[] = [];
 
-  modalPicture: string = "";
-  message: string = "";
-  personName: string = "";
+  modalPicture: string = '';
+  message: string = '';
+  personName: string = '';
 
   isModalOpen = false;
   password = '';
   validPassword = '2025';
 
-
-  constructor(private pictureService: BigPictureService, private router: Router) { }
-
+  constructor(
+    private pictureService: BigPictureService,
+    private router: Router,
+  ) {}
 
   checkPassword(route: string): void {
     if (this.password === this.validPassword) {
@@ -50,36 +53,33 @@ export class AppComponent {
     }
   }
 
-
   toggleSearch(enable: boolean) {
     this.isSearchEnabled = true;
   }
 
-
   onChildActivate(componentRef: any) {
     // Enable search only for certain child components
-    this.isSearchEnabled = componentRef instanceof LesDirigeantsComponent || componentRef instanceof OthersMembersComponent;
-
+    this.isSearchEnabled =
+      componentRef instanceof LesDirigeantsComponent ||
+      componentRef instanceof OthersMembersComponent;
   }
 
   onSearch(): void {
-
     if (this.searchQuery.trim()) {
-      this.searchResults = this.pictureService.getBigPictureByName(this.searchQuery.trim());
-
+      this.searchResults = this.pictureService.getBigPictureByName(
+        this.searchQuery.trim(),
+      );
     }
     if (this.searchResults.length == 0) {
-      this.personName = "Aucun nom trouvé";
-      this.modalPicture = "";
-      this.message = "";
+      this.personName = 'Aucun nom trouvé';
+      this.modalPicture = '';
+      this.message = '';
     } else {
-
       this.modalPicture = this.searchResults[0].imageUrl;
       this.message = this.searchResults[0].message;
       this.personName = this.searchResults[0].name;
     }
   }
-
 
   CloseModel() {
     const modelDiv = document.getElementById('myModal');
@@ -95,4 +95,121 @@ export class AppComponent {
     }
   }
 
+  getRainbowColor(index: number): string {
+    const hue = (index * 25) % 360; // controls color spread
+    return `hsl(${hue}, 70%, 50%)`;
+  }
+
+  worldMusicList = [
+    { name: "Maxwell - Ascension (Don't Ever Wonder)", link: ['/DontEverWonder', 'D7rm9t5S4uE'] },
+    { name: 'After 7 - Ready or Not', link: ['/ReadyOrNot', 'fohpiig8N4U'] },
+    { name: 'George Michael, Aretha Franklin - I Knew You Were Waiting', link: ['/IKnewYouWereWaiting', 'fDxzQJaA228'] },
+    { name: 'Johnny Gill - My My My', link: ['/MyMyMy', 'LMcANn_5UxM'] },
+    { name: "Joe - All The Things (Your Man Won't Do) ", link: ['/AllTheThings', 'cVi25pSRAkc'] },
+    { name: 'The Three Degrees - When Will I See You Again', link: ['/WhenIseeYouAngain', '3R878g-SppU'] },
+    { name: "Love Unlimited Orchestra ~ Love's Theme 1973 Disco", link: ['/LoveTheme', 'RLTJ95kj9ng'] },
+    { name: "Tina - What's Love Got To Do With it", link: ['/TinaWhatLove', 'oGpFcHTxjZs'] },
+    { name: 'Toni Braxton - Long As I Live', link: ['/LongAsILive', 'EjW7WzozgI0'] },
+    { name: 'Patti labelle - On My Own ft', link: ['/OnMyOwnft', 'KsH63qJlIMM'] },
+    { name: 'Percy Faith - The Theme from "A Summer Place"', link: ['/ASummerPlace', '3ofmPuHBCCc'] },
+    { name: 'Haha - Take On Me', link: ['/TakeOnMe', 'djV11Xbc914'] },
+    { name: 'Dieudonne Larose - Accident', link: ['/Accident', '31LxCq1DPDk'] },
+    { name: 'Thomas Frinking: Coastline', link: ['/CoastLine', 'b0i4h4ywO1w'] },
+    { name: "Chaka Khan: Ain't Nobody", link: ['/AinTNobody', 'rcAv1cGAeXE'] },
+    { name: 'Julio Iglesias: Vous les femmes', link: ['/VousLesFemmes', 'FWhwwSX3fEA'] },
+    { name: 'George Michael - Careless Whisper', link: ['/CarelessWhisper', 'izGwDsrQ1eQ'] },
+    { name: 'Sade - Kiss Of Life', link: ['/KissOfLife', 'MmOau-PMWJk'] },
+    { name: 'Francis Bebey - Bana ba Zaire', link: ['/Kinshasa', 'lzZz_yVZ9Y0'] },
+    { name: 'Grace Jones - La Vie En Rose', link: ['/LaVieEnRose', 'E5M7l_z93rg'] },
+    { name: 'EL Negro Y RAY - Carnaval', link: ['/Carnavale', '-A2Wwyuqa6g'] },
+    { name: 'The Temptations - Treat Her Like A Lady', link: ['/LikeALady', '3jqHdZi3EYU'] },
+    { name: 'Foreigner - I want to Know What Love Is', link: ['/WhatToKnow', 'r3Pr1_v7hsw'] },
+    { name: 'Quicy Jones - Summer In The City', link: ['/SummerInCity', 'jpbK4tLQH_s'] },
+    { name: "Claude Barzotti - Prends bien soin d'elle", link: ['/PrendSoinDelle', 'hPZvuowFkx8'] },
+    { name: 'Julio Iglesias - Fideles', link: ['/Fideles', 'cqS-VrUEiHo'] },
+    { name: 'George Benson - Shiver', link: ['/Shiver', '8VLnxe7BBq0'] },
+    { name: 'The EQS - In Our Dream', link: ['/OurDream', 'IQsbmLqNF1A'] },
+    { name: 'Madona - Holiday', link: ['/Holiday', 'h4bP9tj_0Zk'] },
+    { name: 'Whitney with Kenny G - For The Love Of You', link: ['/ForTheLoveOfYou', '_BB48yeqQ8Q'] },
+    { name: 'Kool and The Gang - Fresh', link: ['/Fresh', 'sTJ1XwGDcA4'] },
+    { name: 'S.O.S Band - The Finest', link: ['/TheFinest', 'iK0YkFOD18U'] },
+    { name: 'Cherrelle - Saturday Love', link: ['/SaturdayLove', 'efLcgUQmyT8'] },
+    { name: 'Alphaville - Forever Young', link: ['/ForeverYoung', 'oNjQXmoxiQ8'] },
+    { name: 'Regina Belle - Baby Come To Me', link: ['/BabyComeToMe', 'pQflBV1UgVY'] },
+    { name: 'Kenny Loggins -Love Will Follow', link: ['/LoveWillFollow', 'ANA_hlouklY'] },
+    { name: 'Laura Branigan - Self Control', link: ['/SelfControl', 'RP0_8J7uxhs'] },
+    { name: 'George McCrae: Rock Your Baby', link: ['/RockYourBaby', '6Q15-kql1YE'] },
+
+  ];
+
+  congoMusicList = [
+    { name: 'Hortense', link: ['/Hortense', 'VpvDSz8skb8'] },
+    { name: 'Rtc Riva - Madilu System', link: ['/RTCRIVA', 'owgNTJlBjNQ'] },
+    { name: 'Koukoulou - Koffi', link: ['/Koukoulou', 'j8gCb6bKC44'] },
+    { name: 'Kelhia - Mbilia Bel', link: ['/Kelhiambiliabel', '0SpzfQnFC4Q'] },
+    { name: 'Eswi yo wapi - Mbilia Bel', link: ['/Eswiyowapi', 'gcrkm2HFV-o'] },
+    { name: 'Maze - Rochereau Tabu Ley', link: ['/Maze', 'xUaMEARE-2E'] },
+    { name: 'Beyanga - Mbilia Bel', link: ['/Beyanga', 'ZhXnj3EACXA'] },
+    { name: 'Molunge Ya Mbanda', link: ['/MolungeYaMbanda', '53ck5zij__o'] },
+    {
+      name: 'Bon Samaritain - Papa Noel',
+      link: ['/BonSamaritain', '8bMc6-VWvMM'],
+    },
+    { name: 'Selia zozo - Papa Noel', link: ['/SeliaZozo', 'aU3hSzW3j3c'] },
+    { name: 'Tangawisi', link: ['/Tangawisi', 'Td34YSYubvA'] },
+    { name: 'Prolongation - Karmapa', link: ['/Prolongation', 'k3_odBeWt44'] },
+    { name: 'Ndaya paradis', link: ['/NdayaParadis', 'PSyUYXHN9p4'] },
+    { name: 'Nzele - Madilu System', link: ['/NzeleMadilu', 'FGkEAN9lMzA'] },
+    {
+      name: 'Centieme Dossier - Fally Ipupa',
+      link: ['/CentièmeDossier', 'gIfxaiLOjPQ'],
+    },
+    {
+      name: 'Nzambe mokonzi- Bela Bela',
+      link: ['/Nzambemokonzi', 'CXDRyznygQA'],
+    },
+    { name: 'Joël photo', link: ['/JoelPhoto', '-a_02lS-lg0'] },
+    {
+      name: 'Tokoma ba Camarade Pamba',
+      link: ['/TokomaBaCamarade', 'm7N5ZvrsmjE'],
+    },
+    { name: 'Orgasy - Fally Ipupa', link: ['/Orgasy', 'ihmtjqUyFWQ'] },
+    { name: 'Point Final', link: ['/PointFinal', 'ulznRM_QQPc'] },
+    { name: 'Nzoto Na Nga Se Moko', link: ['/NzotoMoko', 'Po0U1sF45S0'] },
+    { name: 'Maya - Simaro', link: ['/Maya', 'e9cKkBGleGE'] },
+    { name: 'Zéro faute - Koffi', link: ['/ZéroFaute', 'sDCoVhUilys'] },
+    { name: 'Coucou - Koffi', link: ['/Coucou', 'HZNKqR6C1cE'] },
+    { name: 'Ngobila - Koffi', link: ['/Ngobila', 'ka-M15DX0Pw'] },
+    { name: 'Ndaya', link: ['/Ndaya', 'hNco9iib8nM'] },
+    {
+      name: 'Faute Ya Commerçant',
+      link: ['/FauteYaCommerçant', 'uUzK50qrITw'],
+    },
+    { name: 'Mukungu -OK JAZZ', link: ['/Mukungu', 'zveJw24HErM'] },
+    { name: 'DJINO - Koffi', link: ['/Djino', 'xaGTNwnHqXM'] },
+    { name: 'Pesa Position - OK JAZZ', link: ['/PesaPosition', 'iIwgqiSKGCk'] },
+    { name: 'Mabele (Ntoto) - Simaro', link: ['/Mabele', 'RAnITiF0Fgc'] },
+    {
+      name: 'Karibou ya Bintou -Tabu Lay',
+      link: ['/KaribouYaBintou', '8kR_o9daD2I'],
+    },
+    { name: 'Deliya - Ntesa', link: ['/Deliya', 'YNuSZ3EUS10'] },
+    { name: 'Kelya', link: ['/Kelya', '-t6_DXt7jW8'] },
+    { name: 'Longo', link: ['/Longo', 'NzLK08ISCbU'] },
+    { name: 'Lemba Lemba - Lipua Lipua', link: ['/LembaLemba', 'l77Hw47dEDE'] },
+    { name: 'Bonbon sucré', link: ['/BonbonSucré', 'snFPbHQoL7I'] },
+    { name: 'Kamale', link: ['/Kamale', 'rEOppwdr2GI'] },
+    { name: 'Nzale - Tabu Ley', link: ['/Nzale', 'khl2e6to5Co'] },
+    { name: 'Nono - Bella Bella', link: ['/Nono', 'T9CPEkxsNxE'] },
+    { name: 'Monzo - OK JAZZ', link: ['/Monzo', '-njsS91vSac'] },
+    {
+      name: 'Constantation - DJengaka',
+      link: ['/Constantation', '5uBwCIWb108'],
+    },
+    { name: 'Matoba - Lipua Lipua', link: ['/Matoba', 'xhxM5RqqDf0'] },
+    { name: 'Ebale Ya Zaire - Simaro', link: ['/EbaleYaZaire', '6zh721l_6gI'] },
+    { name: 'Jamais Kolonga', link: ['/JamaisKolonga', 'Hhue5hh_IOM'] },
+    { name: 'Maya Version2 - Simaro', link: ['/MayaVersion2', '1oPDUjb7j-o'] },
+    { name: 'Ikea - Koffi', link: ['/Ikea', 'HxDdpBMWI3Q'] },
+  ];
 }
